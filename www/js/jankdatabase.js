225ba65a809bf562c342c41af20641792cebb176
DoumaRDT <rosadouma@MacBook-Pro-van-Rosa.local>
2014-12-15 14:26:43 +0100

    Jankdatabase geïntegreerd

---
+++ www/js/jankdatabase.js
@@ -0,0 +1,26 @@
+var jankdatabase = [
+{
+	"tijd"    : "20131011T10:00+01:00",
+	"locatie" : "Pd 208",
+	"intensiteit": 7,
+	"reden" : "Overvallen door hoeveelheid werk",
+},
+{
+	"tijd"    : "20131011T10:00+01:00",
+	"locatie" : "Pd 208",
+	"intensiteit": 7,
+	"reden" : "Overvallen door hoeveelheid werk",
+},
+{
+	"tijd"    : "20131011T10:00+01:00",
+	"locatie" : "Pd 208",
+	"intensiteit": 7,
+	"reden" : "Overvallen door hoeveelheid werk",
+},
+{
+	"tijd"    : "20131011T10:00+01:00",
+	"locatie" : "Pd 208",
+	"intensiteit": 7,
+	"reden" : "Overvallen door hoeveelheid werk",
+},
+];
