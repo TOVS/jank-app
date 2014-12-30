8a1a26b673a9aa5ac74d1a2a17ee6cb73d50ff62
DoumaRDT <rosadouma@MacBook-Pro-van-Rosa.local>
2014-12-30 20:07:14 +0100

    Jankdatabase Rosa

--- www/js/jankdatabase.js
+++ www/js/jankdatabase.js
@@ -7,28 +7,73 @@ var jankdatabase = [
 },
 {
 	"Naam"	  : "Rosa",
-	"tijd"    : "20131022T10:00+12:00",
+	"tijd"    : "20131022T12:00+01:00",
 	"locatie" : "kamer Pauline",
 	"reden" : "Niet weten waar te beginnen",
 },
 {
 	"Naam"	  : "Rosa",
-	"tijd"    : "20131031T10:00+11:00",
+	"tijd"    : "20131031T11:00+01:00",
 	"locatie" : "PC 212",
 	"reden" : "Werk niet meer kunnen overzien",
 },
 {
 	"Naam"	  : "Rosa",
-	"tijd"    : "20131101T10:00+12:00",
+	"tijd"    : "20131101T12:00+01:00",
 	"locatie" : "kamer pauline",
 	"reden" : "Werk niet meer kunnen overzien",
 },
 {
 	"Naam"	  : "Rosa",
-	"tijd"    : "20131105T10:00+08:30",
+	"tijd"    : "20131105T08:30+01:00",
 	"locatie" : "gang",
 	"reden" : "Frustratie: slecht gewerkt de afgelopen dagen",
 },
+{
+	"Naam"	  : "Rosa",
+	"tijd"    : "20131108T14:00+01:00",
+	"locatie" : "PC212",
+	"reden" : "Werk niet meer kunnen overzien",
+},
+{
+	"Naam"	  : "Rosa",
+	"tijd"    : "20131114T10:00+01:00",
+	"locatie" : "PC212",
+	"reden" : "Paniekerig gevoel",
+},
+{
+	"Naam"	  : "Rosa",
+	"tijd"    : "20131122T15:00+01:00",
+	"locatie" : "PC212",
+	"reden" : "Arts: constatering burnout",
+},
+{
+	"Naam"	  : "Rosa",
+	"tijd"    : "20131128T13:00+01:00",
+	"locatie" : "Kamer Pauline",
+	"reden" : "Wat te doen met burnout",
+},
+{
+	"Naam"	  : "Rosa",
+	"tijd"    : "20131128T13:00+01:00",
+	"locatie" : "Kamer Pauline",
+	"reden" : "besluit per direct te stoppen met het studiejaar",
+},
+{
+	"Naam"	  : "Rosa",
+	"tijd"    : "20140916T11:00+01:00",
+	"locatie" : "Kamer Pauline",
+	"reden" : "Reflectie afgelopen jaar, aanvang stuiedjaar",
+},
+
+{
+	"Naam"	  : "Rosa",
+	"tijd"    : "20141028T13:00+01:00",
+	"locatie" : "Kamer Pauline",
+	"reden" : "Aanvang projectweek: tijdgebrek",
+},
+
+
 
 
 ];
