dc016d9995a002605e5d1338818235329e0e6041
DoumaRDT <rosadouma@MacBook-Pro-van-Rosa.local>
2014-12-30 19:43:03 +0100

    Jankapp update

--- www/js/jankdatabase.js
+++ www/js/jankdatabase.js
@@ -1,26 +1,21 @@
 var jankdatabase = [
 {
+	"Naam"	  : "Rosa",
 	"tijd"    : "20131011T10:00+01:00",
 	"locatie" : "Pd 208",
-	"intensiteit": 7,
 	"reden" : "Overvallen door hoeveelheid werk",
 },
 {
-	"tijd"    : "20131011T10:00+01:00",
-	"locatie" : "Pd 208",
-	"intensiteit": 7,
-	"reden" : "Overvallen door hoeveelheid werk",
+	"Naam"	  : "Rosa",
+	"tijd"    : "20131022T10:00+12:00",
+	"locatie" : "kamer Pauline",
+	"reden" : "Werk niet meer kunnen overzien",
 },
 {
-	"tijd"    : "20131011T10:00+01:00",
-	"locatie" : "Pd 208",
-	"intensiteit": 7,
-	"reden" : "Overvallen door hoeveelheid werk",
+	"Naam"	  : "Rosa",
+	"tijd"    : "20131031T10:00+11:00",
+	"locatie" : "PC 212",
+	"reden" : "Werk niet meer kunnen overzien",
 },
-{
-	"tijd"    : "20131011T10:00+01:00",
-	"locatie" : "Pd 208",
-	"intensiteit": 7,
-	"reden" : "Overvallen door hoeveelheid werk",
-},
+
 ];
