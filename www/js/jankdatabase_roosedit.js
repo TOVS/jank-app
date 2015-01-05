3f94df9fc82f8f82b92011ae2f2837831568a8d4
roosgr <roosgroothuizen@me.com>
2015-01-05 12:22:17 +0100

    Tekst nieuwe taal

--- www/js/jankdatabase_roosedit.js
+++ www/js/jankdatabase_roosedit.js
@@ -2,63 +2,57 @@ var jankdatabase = [
 {
 	"Naam"	  : "Rosa",
 	"tijd"    : "20131011T10:00+01:00",
-	"locatie" : "Pd 208",
-	"reden" : "Overvallen door hoeveelheid werk",
+	"locatie" : "PD 208",
+	"reden" : "Overvallen door de hoeveelheid werk",
 },
 {
 	"Naam"	  : "Rosa",
 	"tijd"    : "20131022T12:00+01:00",
-	"locatie" : "kamer Pauline",
-	"reden" : "Niet weten waar te beginnen",
+	"locatie" : "Kamer Pauline",
+	"reden" : "Niet weten waar ik moet beginnen",
 },
 {
 	"Naam"	  : "Rosa",
 	"tijd"    : "20131105T08:30+01:00",
-	"locatie" : "gang",
-	"reden" : "Frustratie: slecht gewerkt de afgelopen dagen",
+	"locatie" : "Gang bij Pauline",
+	"reden" : "Frustraties, slecht gewerkt de afgelopen dagen",
 },
 {
 	"Naam"	  : "Rosa",
 	"tijd"    : "20131108T14:00+01:00",
-	"locatie" : "PC212",
-	"reden" : "Werk niet meer kunnen overzien",
+	"locatie" : "PC 212",
+	"reden" : "Ik kan het werk niet meer overzien",
 },
 {
 	"Naam"	  : "Rosa",
 	"tijd"    : "20131114T10:00+01:00",
-	"locatie" : "PC212",
-	"reden" : "Paniekerig gevoel",
+	"locatie" : "PC 212",
+	"reden" : "Een paniekerig gevoel",
 },
 {
 	"Naam"	  : "Rosa",
 	"tijd"    : "20131122T15:00+01:00",
-	"locatie" : "PC212",
-	"reden" : "Arts: constatering burnout",
+	"locatie" : "PC 212",
+	"reden" : "De arts constateert een burn-out",
 },
 {
 	"Naam"	  : "Rosa",
 	"tijd"    : "20131128T13:00+01:00",
 	"locatie" : "Kamer Pauline",
-	"reden" : "Wat te doen met burnout",
-},
-{
-	"Naam"	  : "Rosa",
-	"tijd"    : "20131128T13:00+01:00",
-	"locatie" : "Kamer Pauline",
-	"reden" : "besluit per direct te stoppen met het studiejaar",
+	"reden" : "Ik besluit per direct te stoppen met het studiejaar",
 },
 {
 	"Naam"	  : "Rosa",
 	"tijd"    : "20140916T11:00+01:00",
 	"locatie" : "Kamer Pauline",
-	"reden" : "Reflectie afgelopen jaar, aanvang studiejaar",
+	"reden" : "Het nieuwe studiejaar begint en ik kijk terug op het vorige jaar",
 },
 
 {
 	"Naam"	  : "Rosa",
 	"tijd"    : "20141028T13:00+01:00",
 	"locatie" : "Kamer Pauline",
-	"reden" : "Aanvang projectweek: tijdgebrek",
+	"reden" : "Tijdgebrek tijdens de projectweek",
 },
 
 {
@@ -72,49 +66,42 @@ var jankdatabase = [
 	"Naam"	  : "Rosa",
 	"tijd"    : "20141117T13:00+01:00",
 	"locatie" : "PD 208",
-	"reden" : "Coding valt zwaar",
-},
-
-{
-	"Naam"	  : "Rosa",
-	"tijd"    : "20141130T11:00+01:00",
-	"locatie" : "PD 208",
-	"reden" : "Werk niet meer kunnen overzien",
+	"reden" : "Coding valt me zwaar",
 },
 
 {
 	"Naam"	  : "Rosa",
 	"tijd"    : "20141201T14:00+01:00",
-	"locatie" : "kamer pauline",
-	"reden" : "Frustratie: Geen tijd om ziek te zijn",
+	"locatie" : "Kamer pauline",
+	"reden" : "Ik heb geen tijd om ziek te zijn",
 },
 {
 	"Naam"	  : "Rosa",
 	"tijd"    : "20141203T14:10+01:00",
-	"locatie" : "thuis",
-	"reden" : "Niet in staat om te werken",
+	"locatie" : "Ingang",
+	"reden" : "Ik ben niet in staat om te werken",
 },
 
 {
 	"Naam"	  : "Malou",
-	"tijd"    : "20141203T14:10+01:00",
-	"locatie" : "Onbekend",
-	"reden" : "Toen ik dan toch echt mijn definitieve besluit vertelde om later af te studeren tegen Michel en Frits. ",
+	"tijd"    : "20140507T11:07+01:00",
+	"locatie" : "PC 112",
+	"reden" : "Ik vertel Michel en Frits mijn definitieve besluit om later af te studeren",
 },
 
 {
 	"Naam"	  : "Malou",
-	"tijd"    : "20141203T14:10+01:00",
-	"locatie" : "Onbekend",
-	"reden" : "Verheul zei dat dat ik moest overwegen om vrijwillig het 3de jaar overnieuw te doen. ",
+	"tijd"    : "20140630T15:30+01:00",
+	"locatie" : "BB 105",
+	"reden" : "Verheul zegt dat dat ik moet overwegen om vrijwillig het 3de jaar opnieuw te doen",
 },
 
 
 {
 	"Naam"	  : "Roos",
 	"tijd"    : "20140703T21:30+01:00",
-	"locatie" : "gipsenzaal",
-	"reden" : "Ik krijg te horen dat ik ben gezakt.",
+	"locatie" : "Gipsenzaal",
+	"reden" : "Ik krijg te horen dat ik ben gezakt",
 },
 
 
@@ -122,120 +109,148 @@ var jankdatabase = [
 	"Naam"	  : "Roos",
 	"tijd"    : "20140703T13:30+01:00",
 	"locatie" : "PD 205",
-	"reden" : "Dagen en nachten al je energie erin stoppen, wordt je werk volledig afgekraakt door de jury.",
+	"reden" : "Dagen en nachten al je energie erin stoppen, wordt je werk volledig afgekraakt door de jury",
 },
 
 {
 	"Naam"	  : "Roos",
 	"tijd"    : "20140603T12:00+01:00",
 	"locatie" : "Computerzaal",
-	"reden" : "Ik zie het niet meer zitten en niks lukt me te beginnen met werken.",
+	"reden" : "Ik zie het niet meer zitten en niks lukt me te beginnen met werken",
 },
 
 
 {
 	"Naam"	  : "Roos",
 	"tijd"    : "20130129T17:00+01:00",
-	"locatie" : "BB105",
-	"reden" : "Je krijgt een voldoende maar je hebt in zoveel stress geleefd dat niks meer leuk is.",
+	"locatie" : "BB 105",
+	"reden" : "Je krijgt een voldoende maar je hebt in zoveel stress geleefd dat niks meer leuk is",
 },
 
 {
 	"Naam"	  : "Roos",
 	"tijd"    : "20131218T11:00+01:00",
-	"locatie" : "PD208",
-	"reden" : "Je oma heeft een hersenbloeding, niet aan denken, die scriptie heeft voorrang.",
+	"locatie" : "PD 208",
+	"reden" : "Je oma heeft een hersenbloeding, niet aan denken, die scriptie heeft voorrang",
 },
 
 {
 	"Naam"	  : "Orphe",
 	"tijd"    : "20130220T11:00+01:00",
-	"locatie" : "Glasbak, 4de jaars lokaal",
-	"reden" : "Sorry, maar je mag niet afstuderen. (FUCK!)",
+	"locatie" : "PC 212",
+	"reden" : "Ik mag niet afstuderen",
 },
 
 {
 	"Naam"	  : "Orphe",
 	"tijd"    : "20111022T11:00+01:00",
 	"locatie" : "Spiegelgang",
-	"reden" : "Ik ben 1 van de 10 allochtonen op school.",
+	"reden" : "Ik ben 1 van de 10 allochtonen op school",
 },
 
 {
 	"Naam"	  : "Orphe",
 	"tijd"    : "20120127T11:00+01:00",
-	"locatie" : "BB105",
-	"reden" : "Gelukkig heeft iedereen uit de klas net zo een zware kater door gisteravond.",
+	"locatie" : "BB 105",
+	"reden" : "Ik heb zo'n zware kater",
 },
 
 {
 	"Naam"	  : "Orphe",
 	"tijd"    : "20120126T11:00+01:00",
 	"locatie" : "Hal Blijenburg",
-	"reden" : "Grafisch is vanaf nu officeel de party afdeling!",
+	"reden" : "Mijn afdeling gaat alleen maar om feesten en nooit meer om schoolwerk",
 },
 
 {
 	"Naam"	  : "Orphe",
 	"tijd"    : "20110615T11:00+01:00",
-	"locatie" : "Docentenkamer Blijenburg",
-	"reden" : "Vanaf nu ga ik nooooooit meer te stressen.",
+	"locatie" : "Docentenkamer Blijenburg (BB 108?)",
+	"reden" : "Ik wil nooooooit meer te stressen",
 },
 
 {
 	"Naam"	  : "Orphe",
 	"tijd"    : "20120515T11:00+01:00",
 	"locatie" : "WC Blijenburg",
-	"reden" : "Shit, wat zie ik er slecht uit!",
+	"reden" : "Ik zie er slecht uit!",
 },
 
 {
 	"Naam"	  : "Orphe",
 	"tijd"    : "20110312T11:00+01:00",
-	"locatie" : "BB105",
-	"reden" : "School heeft me eerste echte relatie kapot gemaakt",
+	"locatie" : "BB 105",
+	"reden" : "Mijn eerste echte relatie is kapot gemaakt door school",
 },
 
 {
 	"Naam"	  : "Orphe",
 	"tijd"    : "20131201T11:00+01:00",
-	"locatie" : "Glasbak",
+	"locatie" : "PD 208",
 	"reden" : "Nergens kan ik aan de bak als ontwerper",
 },
 
 {
 	"Naam"	  : "Orphe",
 	"tijd"    : "20101125T11:00+01:00",
-	"locatie" : "Lokaal Blijenburg",
-	"reden" : "School is belangerijker dan me vriendin...",
-},
-
-{
-	"Naam"	  : "Orphe",
-	"tijd"    : "20120414T11:00+01:00",
-	"locatie" : "Expohal",
-	"reden" : "Wie is dit leuke meisje?",
+	"locatie" : "BB 105",
+	"reden" : "School is belangrijker dan mijn vriendin",
 },
 
 {
 	"Naam"	  : "Orphe",
 	"tijd"    : "20110618T11:00+01:00",
 	"locatie" : "Docentenkamer Blijenburg",
-	"reden" : "Jaaaaa man! Heb me P!",
-},
-
-{
-	"Naam"	  : "Orphe",
-	"tijd"    : "20110618T11:00+01:00",
-	"locatie" : "Docentenkamer Blijenburg",
-	"reden" : "Jaaaaa man! Heb me P!",
+	"reden" : "Jaaaaa man! Ik heb mijn P!",
 },
 
 {
 	"Naam"	  : "Roos",
 	"tijd"    : "20110618T11:00+01:00",
-	"locatie" : "Hout werkplaats",
+	"locatie" : "Houtwerkplaats",
 	"reden" : "Mijn project valt voor de tiende keer uit elkaar",
+},
+
+{
+	"Naam"	  : "Malou",
+	"tijd"    : "20120129T10:07+01:00",
+	"locatie" : "BB 106",
+	"reden" : "Ik heb er compleet een potje van gemaakt bij mijn eerste Letterstudio beoordeling",
+},
+
+{
+	"Naam"	  : "Malou",
+	"tijd"    : "20131107T14:37+01:00",
+	"locatie" : "PD 208",
+	"reden" : "Ik weet het niet meer met mijn scriptie en ik zie nog 3 andere janken",
+},
+
+{
+	"Naam"	  : "Malou",
+	"tijd"    : "20140327T08:01+01:00",
+	"locatie" : "Kamer Pauline",
+	"reden" : "Ik ben van de trein maar regelrecht doorgelopen naar de kamer van Pauline, ik zie het niet meer zitten",
+},
+
+{
+	"Naam"	  : "Malou",
+	"tijd"    : "20131121T12:52+01:00",
+	"locatie" : "Kantine",
+	"reden" : "In de kantine pratend over school",
+},
+
+{
+	"Naam"	  : "Malou",
+	"tijd"    : "20140128T13:23+01:00",
+	"locatie" : "PD 208",
+	"reden" : "Ik ben te zenuwachtig vlak voor de collectieve beoordelingen",
+},
+
+{
+	"Naam"	  : "Malou",
+	"tijd"    : "20140514T16:05+01:00",
+	"locatie" : "WC glasbak",
+	"reden" : "Ik denk teveel op de wc",
 },
 
 ];
