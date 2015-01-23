864bbd77b363db4b50b3b6fe617ce0da3a68700e
roosgr <roosgroothuizen@me.com>
2015-01-23 10:41:33 +0100

    App CSS + javascript

--- www/js/index.js
+++ www/js/index.js
@@ -4,17 +4,36 @@ var whereAreWe = function() {
     navigator.geolocation.getCurrentPosition(updatePosition, onError);
 };
 
+	// positie device opvragen
 var updatePosition = function(position) {
     var $results = $('#results');
-    $results.html( "latitude " + position.coords.latitude + ", longitude " + position.coords.longitude  );
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    $results.html( latitude + ", " + longitude  );
     
+    // loadscreen hidden maken en content visible maken
     $("#loading").addClass("hidden");
-    $results.removeClass("hidden");
+    $("#oog").removeClass("hidden");
+    $("#container").removeClass("hidden");
     
-    var $database = $('#database');
-    $database.html(JSON.stringify(jankdatabase, null, 2));
-    $database.removeClass("hidden");
+    // ik weet niet wat dit doet
+    for (var i = 0; i < jankdatabase.length; i++) {
+	    var jankmoment = jankdatabase[i];
+	    console.log(jankmoment.reden);
+    }
+    
+    
+    // Op dit moment wordt alleen object 3 uit de JSON gehaald en is er niks op basis van coords
+    document.getElementById('reden').innerHTML = jankdatabase[3].reden;    
+    document.getElementById('tijd').innerHTML = jankdatabase[3].tijd;
+    document.getElementById('locatie').innerHTML = jankdatabase[3].locatie;
+    
+    // document.getElementById('naam').innerHTML = jankdatabase[3].Naam + " huilt:";
+
+    
+    
 };
+
 
 var onError = function(error) {
     // this code is run if the app fails in finding out where we are
